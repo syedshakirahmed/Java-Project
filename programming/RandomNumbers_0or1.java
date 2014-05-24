8ead56b318347ac64d3cfbe6ed4e33d882a08f09
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-05-24 19:43:53 +0530

    random integer either 0 or 1

---
+++ programming/RandomNumbers_0or1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class RandomNumbers_0or1{
+    public static void main(String args[]){
+        //result either 0 or 1
+        int r = (int)(Math.random()*2);
+        System.out.println("Result either 0 or 1: "+r);
+    }
+}
