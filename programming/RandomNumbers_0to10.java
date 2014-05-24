4124d91b2b397e4661b6b42506d1a3e124dfc4c0
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-05-24 19:44:03 +0530

    random integer from 0 to 10

---
+++ programming/RandomNumbers_0to10.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class RandomNumbers_0to10{
+    public static void main(String args[]){
+        //result from 0 to 10
+        int r = (int)(Math.random()*11);
+        System.out.println("Result from 0 to 10: "+r);
+    }
+}
