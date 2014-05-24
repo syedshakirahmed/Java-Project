42895233e6a7a56584a151e05356e138fd394c29
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-05-24 19:43:43 +0530

    random number

---
+++ programming/RandomNumber.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class RandomNumber{
+    public static void main(String args[]){
+        double r = Math.random();
+        System.out.println("Random No: "+r);
+    }
+}
